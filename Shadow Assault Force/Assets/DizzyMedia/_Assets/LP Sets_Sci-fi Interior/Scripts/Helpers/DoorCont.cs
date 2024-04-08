465c5d0a8cc56a51ec64c4a5dae786c188db0c18
RonCohen47 <138518157+RonCohen47@users.noreply.github.com>
2024-04-08 18:42:22 +0300

    Built 5 Levels

--- Shadow Assault Force/Assets/DizzyMedia/_Assets/LP Sets_Sci-fi Interior/Scripts/Helpers/DoorCont.cs
+++ Shadow Assault Force/Assets/DizzyMedia/_Assets/LP Sets_Sci-fi Interior/Scripts/Helpers/DoorCont.cs
@@ -78,7 +78,7 @@ public class DoorCont : MonoBehaviour {
     
     private void OnTriggerEnter(Collider other) {
         
-        if(!locked && PlayerController.EnemyCount == 0){
+        if(!locked && PlayerController.EnemyCount <= 0){
         
             if(other.gameObject.tag == tagToCheck){
 
